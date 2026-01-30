562a486fc0b8080ffde4add5ad88cdfad61ec130
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2026-01-30 14:36:32 -0500

    Lab #1
    Completed Lab #1

--- Labs/Helper.h
+++ Labs/Helper.h
@@ -1,1 +1,41 @@
+#ifndef HELPER_H
+#define HELPER_H
 
+#include <iostream>
+#include <string>
+
+// IsBinaryString function
+bool IsBinaryString(const std::string &s)
+{
+	if (s.length() == 0)
+	{
+		return false;
+	}
+	for (char c : s)
+	{
+		if (c != '0' && c != '1')
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+// ToInteger function
+unsigned int ToInteger(const std::string &s)
+{
+	if (s.length() > 10 || !IsBinaryString(s))
+	{
+		return 0;
+	}
+
+	unsigned int converted = 0;
+	for (char c : s)
+	{
+		converted = converted * 2 + (c - '0');
+	}
+	return converted;
+}
+
+#endif
