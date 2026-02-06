de3a752042b4bdb2dc0a04d6ed87bb4059f134b2
ReidConsole <jermainereid@mec.science>
2026-02-06 17:36:04 -0500

    Notes Update

---
+++ Notes/Note01/Solutions/main.cpp
@@ -0,0 +1,19 @@
+#include "Memory.h"
+
+int main()
+{
+    //Testing istream and toString()
+    can::Memory mem;
+    std::cout << "Enter memory content: ";
+    std::cin >> mem;
+    std::cout << mem.read() << "\n";
+    std::cout << mem << "\n";
+    std::cout << can::toDecimal(mem.read()) << "\n";
+    
+    int x;
+    std::cout << "Enter an integer: ";
+    std::cin >> x;
+    std::cout << can::toBinary(x) << "\n";
+    
+    return 0;
+}
