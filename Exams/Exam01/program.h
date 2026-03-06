c934a1b48463499e6acbe1e94d953c9b6a4ddaa1
Ox2bc <45404918+Ox2bc@users.noreply.github.com>
2026-03-06 16:09:33 -0500

    Update program.h

--- Exams/Exam01/program.h
+++ Exams/Exam01/program.h
@@ -7,9 +7,81 @@
 #include "Word.h"
 #include "IAS.h"
 
-namespace cae 
+namespace cae
 {
+	// TASK #1
+	Word negate(const Word &word)
+	{
+		if (word.empty())
+		{
+			return word;
+		}
 
+		size_t size = word.size(); // get size of inputed word
+		Word negated(size);		   // create new same sized word
+
+		for (size_t i = 0; i < size; ++i)
+		{
+			negated.set(i, !word.get(i)); // set each bit to the opposite of original bit
+		}
+
+		bool carrybit = true;
+		for (size_t i = 0; i < size && carrybit == true; ++i)
+		{
+			bool current = negated.get(i);
+			negated.set(i, current ^ carrybit);
+			carrybit = current && carrybit;
+		}
+
+		return negated;
+	}
+
+	// TASK #2
+	void LDM(IAS &obj)
+	{
+		int address = value(obj(3));
+		transfer(obj[address], obj(5));
+	}
+
+	// TASK #3
+	void LSH(IAS &obj)
+	{
+		Word &ac = obj(5);
+		size_t size = ac.size();
+		for (size_t i = size - 1; i > 0; --i)
+		{
+			ac.set(i, ac.get(i - 1));
+		}
+		ac.set(0, false);
+	}
+
+	// TASK #4
+	bool ADD(IAS &obj)
+	{
+		Word &A = obj(5);
+		Word &B = obj(4);
+
+		size_t sizeA = A.size();
+		bool carry = false;
+		bool signA = A.get(sizeA - 1);
+		bool signB = B.get(sizeA - 1);
+
+		for (size_t i = 0; i < sizeA; ++i)
+		{
+			bool bitA = A.get(i);
+			bool bitB = B.get(i);
+			A.set(i, bitA ^ bitB ^ carry);
+			carry = (bitA && bitB) || (carry && (bitA ^ bitB));
+		}
+
+		bool overflow = false;
+		if ((signA == signB) && (A.get(sizeA - 1) != signA))
+		{
+			overflow = true;
+		}
+
+		return overflow;
+	}
 }
 
 #endif
